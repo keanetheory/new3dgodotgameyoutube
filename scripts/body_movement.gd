c59817e7752cd13ce06010103677dbf0b6d227d9
cynicelt <pmkeane@gmail.com>
2026-09-17 18:26:55 +0100

    pushing up a little code refinement

--- scripts/body_movement.gd
+++ scripts/body_movement.gd
@@ -1,21 +1,26 @@
 extends CharacterBody3D
 
+# Determins how fast a player moves
+@export var SPEED = 10
+const JUMP_VELOCITY = 10
 
-const SPEED = 15.0
-const JUMP_VELOCITY = 5
+func get_boosted_speed(booster):
+	return SPEED * booster
 
+func _ready():
+	print(get_boosted_speed(2))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+#
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -24,5 +29,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+#
 	move_and_slide()
